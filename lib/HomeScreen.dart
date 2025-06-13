01f48559333f5f4e5ccc083955e932eb35bf97a0
Tu <doanhtu072002@gmail.com>
2025-06-13 10:02:04 +0700

    update

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'StartScreen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const StartScreen()),
+                );
+              },
+              child: const Text('Start Game'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
